988ca61051f4c6c930bd09afe0fa7c98fd02a2c6
vb64 <vit.sar68@gmail.com>
2024-12-13 23:18:34 +0400

    test tpo.set_titul method

--- tests/test/test_tpo.py
+++ tests/test/test_tpo.py
@@ -2,6 +2,8 @@
 
 make test T=test_tpo.py
 """
+from datetime import datetime
+import pytest
 from . import TestBase
 
 
@@ -45,3 +47,24 @@ class TestTpo(TestBase):
         assert "критический" in get_abc('A')
         assert "ремонт" in get_abc('B')
         assert "малозначительный" in get_abc('')
+
+    def test_set_titul(self):
+        """Check set_titul method."""
+        from pipeline_gazprom_infotech.tpo import Infotech
+        from pipeline_gazprom_infotech import InfotechError
+        from pipeline_gazprom_infotech.codes.pypeline import PIPELINE_BY_NAMES
+
+        xml = Infotech()
+
+        with pytest.raises(InfotechError) as err:
+            xml.set_titul("wrong_pipeline_name", None, None, "", "", "", "", {})
+        assert 'не найден в словаре' in str(err.value)
+
+        pipeline_name = list(PIPELINE_BY_NAMES.keys())[0]
+        with pytest.raises(InfotechError) as err:
+            xml.set_titul(pipeline_name, None, None, "", "", "", "", {})
+        assert 'дата начала' in str(err.value)
+        assert 'дата окончания' in str(err.value)
+
+        now = datetime.now()
+        assert xml.set_titul(pipeline_name, now, now, "", "", "", "", {}) is None
